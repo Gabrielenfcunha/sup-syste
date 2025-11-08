93d107adcb7e19ff4f2d43f567421e14365cabae
Gabrielenfcunha <gabifcunha15@gmail.com>
2025-11-08 13:40:04 -0300

    teste

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Index (params) {
   return (
     <div>
-      oi
+      <h1>SUP - Sistema Único de Pets</h1>
       <button><Link href = '/auth/Login'>Login</Link></button>
       <button><Link href = '/auth/SignUp'>SignUp</Link></button>
     </div>
